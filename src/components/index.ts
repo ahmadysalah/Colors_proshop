42f3dffa9a7773dba8d74f33daf8066607cef9fc
ahmadysalah <a.ahmad.salah@gmail.com>
2022-01-18 20:27:48 +0200

    update product screen

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,3 +8,4 @@ export { default as Rating } from './Rating';
 export { Navbar } from './Navbar';
 export { default as Typography } from './Typography';
 export { categoryCard, productCard } from './Card';
+export { default as Counter } from './Counter';
