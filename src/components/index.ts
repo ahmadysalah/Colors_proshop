ce22034eeb0416d50d41b798b7533d3ff4d01993
Your Name <you@example.com>
2022-01-19 16:46:32 +0200

    create PathNavigate component

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,3 +9,4 @@ export { Navbar } from './Navbar';
 export { default as Typography } from './Typography';
 export { categoryCard, productCard } from './Card';
 export { default as Counter } from './Counter';
+export { default as PathNavigate } from './PathNavigate';
