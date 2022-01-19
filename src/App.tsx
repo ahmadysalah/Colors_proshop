1112a234ff59b4734201c03432095a5ebc2b1e0e
mohammedali988 <mohd.os.1998@gmail.com>
2022-01-19 17:03:24 +0200

    Card props

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
-import { CategoryCard } from './components/Card';
-import ProductOverview from './pages/ProductDetails';
+import { Suspense } from 'react';
+import { AllRouter } from './AllRouter';
+import ErrorBoundary from './components/ErrorBoundary';
+import { SpinnerContainer } from './components/SpinnerContainer';
+import { Providers } from './providers';
 
 const App: React.FC = () => {
-  return <CategoryCard />;
+  return (
+    <Providers>
+      sss
+      <Suspense fallback={<SpinnerContainer />}>
+        <ErrorBoundary>
+          <AllRouter />
+        </ErrorBoundary>
+      </Suspense>
+    </Providers>
+  );
 };
 
 export default App;
--- src/components/Card/productCard.tsx
+++ src/components/Card/productCard.tsx
@@ -1,18 +1,30 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button } from '../Button/ButtonStyle';
-import img from '../../assets/tow.jpg';
 import Typography from '../Typography';
 import { Content, ContentAction, Discount, MainCard } from './cardStyles';
+import { IComplexProps } from './interface';
 
-const ComplexCard = () => {
+const ComplexCard = ({
+  img,
+  valueOfDiscount,
+  description,
+  price,
+}: IComplexProps) => {
   const [disCount, setDisCount] = useState(false);
+  const [priceAfterDis, setPriceAfterDis] = useState(0);
 
+  useEffect(() => {
+    if (valueOfDiscount) {
+      setPriceAfterDis(() => price - valueOfDiscount);
+      setDisCount(true);
+    }
+  });
   return (
     <MainCard width="520px" height="655px">
       {disCount ? (
         <Discount>
           <Typography width="none" color="white" fontSize="24px">
-            30%
+            {`${valueOfDiscount}`}
           </Typography>
         </Discount>
       ) : null}
@@ -20,19 +32,19 @@ const ComplexCard = () => {
         <img src={img} alt="" style={{ width: '100%', height: '100%' }} />
       </ContentAction>
       <Content>
-        <Typography variant="p">Description for the product</Typography>
+        <Typography variant="p">{description}</Typography>
       </Content>
       <Content>
         {disCount ? (
           <Typography variant="h2" margin="0 10px" color="red">
-            $999
+            {`${priceAfterDis}`}
           </Typography>
         ) : null}
         <Typography
           variant="h2"
           text-decoration={disCount ? 'line-through' : 'none'}
         >
-          $999
+          {`${price}`}
         </Typography>
       </Content>
       <ContentAction>
