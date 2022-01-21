4dd2c84901a566850279ff68a45b085f0f875485
ahmadysalah <a.ahmad.salah@gmail.com>
2022-01-21 16:03:18 +0200

    fix bug at call elments

--- src/pages/Gest/ProductDetails/index.tsx
+++ src/pages/Gest/ProductDetails/index.tsx
@@ -1,14 +1,15 @@
 import ProductOverview from './Sections/ProductOverview';
 import Specifications from './Sections/Specifications';
 import Review from './Sections/Review';
+import { Container } from '../../../components';
 
 const ProductScreen: React.FC = () => {
   return (
-    <>
+    <Container direction="column">
       <ProductOverview />
       <Specifications />
       <Review />
-    </>
+    </Container>
   );
 };
 
