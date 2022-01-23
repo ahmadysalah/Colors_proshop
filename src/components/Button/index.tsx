07417e649700735e649a754b7c835ac1c32124aa
ahmadysalah <a.ahmad.salah@gmail.com>
2022-01-23 20:56:32 +0200

    update payment

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,5 +2,5 @@ import { Button } from './ButtonStyle';
 import { IStyleButton } from './IStyleButton';
 
 export default function ButtonComponent(props: IStyleButton) {
-  return <Button {...props} />;
+  return <Button {...(props as any)} />;
 }
