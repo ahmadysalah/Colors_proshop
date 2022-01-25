de8888b8bde3f57e84ff51ebf550d5bc400d5256
ahmadysalah <a.ahmad.salah@gmail.com>
2022-01-25 17:28:59 +0200

    update router

--- src/AllRouter.tsx
+++ src/AllRouter.tsx
@@ -9,6 +9,15 @@ const Login = Loadable(lazy(() => import('./pages/Auth/Login')));
 const Product = Loadable(lazy(() => import('./pages/Gest/ProductDetails')));
 const ReviewOrder = Loadable(lazy(() => import('./pages/User/ReviewOrder')));
 const Home = Loadable(lazy(() => import('./pages/Gest/Home')));
+const Cart = Loadable(lazy(() => import('./pages/Cart'))); // bug
+const Dashboard = Loadable(lazy(() => import('./pages/DashBoard'))); // working
+const AddNewProduct = Loadable(lazy(() => import('./pages/NewProduct'))); // bug
+const Profile = Loadable(lazy(() => import('./pages/Profile'))); // bug
+const ProductDetails = Loadable(
+  lazy(() => import('./pages/Gest/ProductDetails')),
+); // bug
+const Search = Loadable(lazy(() => import('./pages/Gest/Search'))); // bug
+const PaymentSuccess = Loadable(lazy(() => import('./pages/PaymentSuccess'))); // need style
 
 export const AllRouter = () => {
   return (
@@ -18,6 +27,13 @@ export const AllRouter = () => {
       <Route path="/Signup" element={<Signup />} />
       <Route path="/product/:id" element={<Product />} />
       <Route path="/review" element={<ReviewOrder />} />
+      <Route path="/cart" element={<Cart />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/addNewProduct" element={<AddNewProduct />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/productDetails/:id" element={<ProductDetails />} />
+      <Route path="/search" element={<Search />} />
+      <Route path="/paymentSuccess" element={<PaymentSuccess />} />
     </Routes>
   );
 };
